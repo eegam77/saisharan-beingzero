ee7a55018b12ce619751fabc2dca0ac243f35063
eegam77 <esaisharan@gmail.com>
2019-12-20 15:39:55 +0530

    a

--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -1,6 +1,6 @@
 
 $.getJSON('/data', function(d){
-    //alert(JSON.stringify(d));
+    alert(JSON.stringify(d));
     $('#h1').text(d.name);
     $('#h2').text(d.college);
     $('#regno').text(d.regno);
